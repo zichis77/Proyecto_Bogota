1b8b765bfe464dca4698baf8634f01c327c929bb
Zayra <zichis@gmail.com>
2020-08-30 10:16:50 -0500

    Se agrega diagnostico de los pacientes a la lista

--- code.js
+++ code.js
@@ -2,20 +2,20 @@ const min = 1000;
 const max = 1500;
 const consulting_room = ['Consultorio 1','Consultorio 2','Consultorio 3','Consultorio 4'];
 const  pacientes = {paciente1:'María Cano', paciente2: 'Camilo Antonio', paciente3: 'Juana Camargo', paciente4: 'Miguel Moreno'};
-
+const diagnostico = {diagnostico1: 'Lumbalgia', diagnostico2: 'Gastroenteritis', diagnostico3: 'Urolitiasis', diagnostico4: 'Cefalea'};
 
 function randomTime (min, max)
 {
   return Math.floor(Math.random()*(max - min)) + min;
 }
 
-var orders = (time, patient, room, diagnostico, conducta, edad) =>
+var orders = (time, patient, room, diagnostico) =>
 {
-  document.write(`<br> Consultorio: ${room}; <br>Paciente ${patient} <br>Tiempo de Consulta: ${time / 100} <br>`);
+  document.write(`<br> Consultorio: ${room}; <br>Paciente ${patient} <br>Tiempo de Consulta: ${time / 100} <br>Diagnostico: ${diagnostico} <br>`);
   return new Promise((resolve, reject) =>{
     setTimeout(() => 
     {
-      resolve(`<br>Paciente atendido: ${patient};<br>Consultorio: ${room}`)
+      resolve(`<br>Paciente atendido: ${patient};<br>Consultorio: ${room} <br>Diagnostico: ${diagnostico}`)
       reject(time, patient, room, diagnostico);
     })
   }
@@ -24,28 +24,28 @@ var orders = (time, patient, room, diagnostico, conducta, edad) =>
 
 const doctor1 = () =>
 {
-  orders (randomTime (min, max), pacientes.paciente1, consulting_room[Math.floor(Math.random()*4)]).then((res) => console.log(res))
+  orders (randomTime (min, max), pacientes.paciente1, consulting_room[Math.floor(Math.random()*4)], diagnostico.diagnostico1).then((res) => console.log(res))
 };
 
 doctor1();
 
 const doctor2 = () =>
 {
-  orders (randomTime (min, max), pacientes.paciente2, consulting_room[Math.floor(Math.random()*4)]).then((res) => console.log(res))
+  orders (randomTime (min, max), pacientes.paciente2, consulting_room[Math.floor(Math.random()*4)], diagnostico.diagnostico2).then((res) => console.log(res))
 };
 
 doctor2();
 
 const doctor3 = () =>
 {
-  orders (randomTime (min, max), pacientes.paciente3, consulting_room[Math.floor(Math.random()*4)]).then((res) => console.log(res))
+  orders (randomTime (min, max), pacientes.paciente3, consulting_room[Math.floor(Math.random()*4)], diagnostico.diagnostico3).then((res) => console.log(res))
 };
 
 doctor3();
 
 const doctor4 = () =>
 {
-  orders (randomTime (min, max), pacientes.paciente4, consulting_room[Math.floor(Math.random()*4)]).then((res) => console.log(res))
+  orders (randomTime (min, max), pacientes.paciente4, consulting_room[Math.floor(Math.random()*4)], diagnostico.diagnostico4).then((res) => console.log(res))
 };
 
 doctor4();
